7042fa140df1ffcb4a6f2a4fb2bfe04c2402da94
Gustavo Inacio <incio.gusta@gmail.com>
2023-09-15 22:26:32 -0300

    check stackdriver logs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,54 @@
-use poem::{get, handler, listener::TcpListener, web::Path,  Route, Server};
+use poem::{get, handler, listener::TcpListener, web::Path,  Route, Server, http::{Uri, Method}};
+use tracing::{error, info, warn};
+use tracing_subscriber::{Registry, prelude::*};
 
 #[handler]
-fn hello(Path(name): Path<String>) -> String {
+fn hello(Path(name): Path<String>, method: Method, uri: &Uri) -> String {
+    info!(
+        http_request.request_method = %method,
+        http_request.request_url = %uri,
+        user = name, 
+        route = "/hello/:name"
+    );
+
     format!("hello: {}", name)
 }
 
+#[handler]
+fn error(Path(name): Path<String>, method: Method, uri: &Uri) -> String {
+    error!(
+        http_request.request_method = %method,
+        http_request.request_url = %uri,
+        user = name, 
+    );
+    format!("hello: {}", name)
+}
+
+
+#[handler]
+fn warning(Path(name): Path<String>, method: Method, uri: &Uri) -> String {
+    warn!(
+        http_request.request_method = %method,
+        http_request.request_url = %uri,
+        user = name, 
+    );
+    format!("hello: {}", name)
+}
+
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = Route::new().at("/hello/:name", get(hello));
+    let stackdriver = tracing_stackdriver::layer(); // writes to std::io::Stdout
+    let subscriber = Registry::default().with(stackdriver);
+
+    tracing::subscriber::set_global_default(subscriber).expect("Could not set up global logger");
+    info!("{}", std::env::var("K_SERVICE").unwrap_or_else(|_| "Not wrapped".to_string()));
+
+
+    let app = Route::new()
+        .at("/hello/:name", get(hello))
+        .at("/warning/:name", get(warning))
+        .at("/error/:name", get(error));
     let port: u16 = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string()).parse()?;
     Server::new(TcpListener::bind(("0.0.0.0", port)))
         .run(app)
